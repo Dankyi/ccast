c6a2c097bab1d64a4b5afd259b8e795638a2eafc
UggUggDev <10110halp@gmail.com>
2022-01-02 21:12:50 +0000

    Initialise ticking AI testing

---
+++ ccast-ai-testing/Ticking-AI-Testing.py
@@ -0,0 +1,20 @@
+"""
+
+Author: Zac King (U1758957)
+
+This Python file is designed to test a more real-time implementation of a neural network, it gathers real-time price
+data for a coin (bound to the rate limit of the exchange) and predicts after a certain amount of time has passed, and
+then repeats.
+
+"""
+
+import ccxt.async_support as ccxt
+import asyncio
+
+
+async def main():
+    pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
