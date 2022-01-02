3bc1493ffaa38e3a0c94c5d4f7455a9ae0afdc1c
UggUggDev <10110halp@gmail.com>
2022-01-02 21:14:16 +0000

    Add function to get exchange

--- ccast-ai-testing/Ticking-AI-Testing.py
+++ ccast-ai-testing/Ticking-AI-Testing.py
@@ -12,8 +12,21 @@ import ccxt.async_support as ccxt
 import asyncio
 
 
+async def get_exchange():
+
+    exchange = ccxt.hitbtc({"verbose": False, "enableRateLimit": True})
+    await exchange.load_markets(True)
+
+    return exchange
+
+
 async def main():
-    pass
+
+    exchange = await get_exchange()
+
+
+
+    await exchange.close()
 
 
 if __name__ == "__main__":
