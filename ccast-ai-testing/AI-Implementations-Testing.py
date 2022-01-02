2863d968abfe7c81922236d0d29dd6c52e25540d
UggUggDev <10110halp@gmail.com>
2022-01-02 15:15:12 +0000

    Initialise common functions (plotting, main) and a couple of starter prediction functions

---
+++ ccast-ai-testing/AI-Implementations-Testing.py
@@ -0,0 +1,38 @@
+"""
+
+Author: Zac King (U1758957)
+
+This Python file is designed to load the price history text file, predict the next price using AI
+and/or machine learning, and graph the output for nicer visualisation.
+
+Implement as many different AIs and/or machine learning algorithms as you want and test them all here.
+
+"""
+
+from sklearn.svm import SVR, NuSVR
+import matplotlib.pyplot as plt
+import numpy as np
+from random import randint
+
+
+def predict_price_svr():
+    pass
+
+
+def predict_price_nusvr():
+    pass
+
+
+# # # AI/Machine Learning implementations go above here... # # #
+
+
+def plot_graph():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
