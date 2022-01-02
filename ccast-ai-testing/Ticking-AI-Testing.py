f2663a78ac689441b7524402ebeb175dc0e24c77
UggUggDev <10110halp@gmail.com>
2022-01-02 23:34:48 +0000

    SVR prediction used instead of ML (more stable right now), both real-time and predicted coin prices are plotted onto a graph at the end

--- ccast-ai-testing/Ticking-AI-Testing.py
+++ ccast-ai-testing/Ticking-AI-Testing.py
@@ -6,11 +6,14 @@ This Python file is designed to test a more real-time implementation of a neural
 data for a coin (bound to the rate limit of the exchange) and predicts after a certain amount of time has passed, and
 then repeats.
 
+The result is shown as a graph at the end.
+
 """
 
-from sklearn.neural_network import MLPRegressor
+from sklearn.svm import NuSVR
 import numpy as np
 import matplotlib.pyplot as plt
+from time import perf_counter_ns as stopwatch
 import ccxt.async_support as ccxt
 import asyncio
 
@@ -35,11 +38,39 @@ def predict_next_price(price_list):
 
     data_points = [[i] for i in range(len(price_list))]
 
-    ml_perceptron = MLPRegressor(solver="lbfgs", hidden_layer_sizes=(8, 2), activation="relu")
-    ml_perceptron.fit(data_points, price_list)
-    predicted_price = ml_perceptron.predict([[len(data_points) + 1]])
+    nu_svr = NuSVR(kernel="rbf")
+    nu_svr.fit(data_points, price_list)
+    predicted_price = nu_svr.predict([[len(data_points) + 1]])
 
     return predicted_price
+
+
+def plot_graph(plot_data, data_points):
+
+    """
+
+    -> RED graph is real-time coin data, default is BTC/USDT.
+    -> GREEN graph is predicted coin data at each interval.
+
+    It's a bit spooky how well the prediction works already, it tends to fit the real graph really well!!
+
+    """
+
+    plt.style.use("seaborn-whitegrid")
+
+    x_axis = [x for x in range(len(plot_data[0]))]
+    x_axis_predicted_points = []
+
+    i = data_points
+
+    for _ in range(len(plot_data[1])):
+        x_axis_predicted_points.append(i)
+        i += data_points
+
+    plt.plot(x_axis, np.array(plot_data[0]), color="red")
+    plt.plot(x_axis_predicted_points, np.array(plot_data[1]), marker='o', color="green")
+
+    plt.show()
 
 
 async def main():
@@ -56,7 +87,7 @@ async def main():
 
     if coin_index > -1:
 
-        data_points = 10  # How many points to gather before training + prediction
+        data_points = 15  # How many points to gather before training + prediction
                           # Note that the rate limit of HitBTC is 1.5 seconds, so 10 will take 15 seconds!!
                           # Add 1.5 seconds to the end also, for the next real price for comparison.
 
@@ -64,23 +95,45 @@ async def main():
                                 # This is temporary for testing, in the real product this would loop until interrupted
                                 # E.g., from the user telling the program to stop
 
-        for _ in range(prediction_amount):
+        plot_data = [[], []]
+
+        for n in range(prediction_amount):
+
+            print("Predicting Price " + str(n + 1) + " / " + str(prediction_amount))
+
+            print()  # Empty print to look nice
 
             price_list = []
 
+            #  It will say, e.g., 11/10, because the last point isn't used in training ..
+            #  .. It's used to compare the next real price with the next predicted price.
             for i in range(data_points + 1):
                 price_list.append(await get_last_price(exchange, coin_index))
-                print("Gathered " + str(i + 1) + " / " + str(data_points + 1) + " ...")
+                print("Gathered " + str(i + 1) + " / " + str(data_points) + " ...")
+
+            s_time = stopwatch()
 
             predicted_price = predict_next_price(price_list[:-1])
+
+            plot_data[0] += price_list
+            plot_data[1].append(predicted_price)
+
+            e_time = stopwatch() - s_time
+            e_time /= 1_000_000.0  # Nanoseconds to Milliseconds
 
             print()  # Empty print to make it look nicer
 
             print("Current Price List: " + str(price_list))
             print("Next Real Price: " + str(price_list[-1]))
-            print("Predicted Real Price: " + str(predicted_price))
+            print("Predicted Real Price: " + str(predicted_price[0]))
 
             print()  # Empty print to make it look nicer
+
+            print("AI Training & Prediction Time Taken: " + str(e_time) + "ms")
+
+            print()  # Empty print to make it look nicer
+
+        plot_graph(plot_data, data_points + 1)
 
     await exchange.close()
 
