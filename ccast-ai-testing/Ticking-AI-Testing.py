d537f47afaa9a029f5520d88b39e689e2bcc9e2f
UggUggDev <10110halp@gmail.com>
2022-01-02 22:11:08 +0000

    ML prediction implemented

--- ccast-ai-testing/Ticking-AI-Testing.py
+++ ccast-ai-testing/Ticking-AI-Testing.py
@@ -8,6 +8,9 @@ then repeats.
 
 """
 
+from sklearn.neural_network import MLPRegressor
+import numpy as np
+import matplotlib.pyplot as plt
 import ccxt.async_support as ccxt
 import asyncio
 
@@ -28,6 +31,17 @@ async def get_last_price(exchange, coin_index):
     return last_price
 
 
+def predict_next_price(price_list):
+
+    data_points = [[i] for i in range(len(price_list))]
+
+    ml_perceptron = MLPRegressor(solver="lbfgs", hidden_layer_sizes=(8, 2), activation="relu")
+    ml_perceptron.fit(data_points, price_list)
+    predicted_price = ml_perceptron.predict([[len(data_points) + 1]])
+
+    return predicted_price
+
+
 async def main():
 
     exchange = await get_exchange()
@@ -43,7 +57,8 @@ async def main():
     if coin_index > -1:
 
         data_points = 10  # How many points to gather before training + prediction
-                          # Note that the rate limit of HitBTC is 1.5 seconds, so 10 will take 15 seconds!
+                          # Note that the rate limit of HitBTC is 1.5 seconds, so 10 will take 15 seconds!!
+                          # Add 1.5 seconds to the end also, for the next real price for comparison.
 
         prediction_amount = 10  # How many predictions to be made
                                 # This is temporary for testing, in the real product this would loop until interrupted
@@ -53,11 +68,19 @@ async def main():
 
             price_list = []
 
-            for i in range(data_points):
+            for i in range(data_points + 1):
                 price_list.append(await get_last_price(exchange, coin_index))
-                print("Gathered " + str(i + 1) + " / " + str(data_points) + " ...")
+                print("Gathered " + str(i + 1) + " / " + str(data_points + 1) + " ...")
 
-            print(price_list)
+            predicted_price = predict_next_price(price_list[:-1])
+
+            print()  # Empty print to make it look nicer
+
+            print("Current Price List: " + str(price_list))
+            print("Next Real Price: " + str(price_list[-1]))
+            print("Predicted Real Price: " + str(predicted_price))
+
+            print()  # Empty print to make it look nicer
 
     await exchange.close()
 
