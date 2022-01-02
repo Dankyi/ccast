6d8b2bacc1962c24cb855fe22530fa6f37d4efb2
UggUggDev <10110halp@gmail.com>
2022-01-02 22:02:05 +0000

    Gathering last price implemented

--- ccast-ai-testing/Ticking-AI-Testing.py
+++ ccast-ai-testing/Ticking-AI-Testing.py
@@ -20,11 +20,44 @@ async def get_exchange():
     return exchange
 
 
+async def get_last_price(exchange, coin_index):
+
+    tick = await exchange.fetch_ticker(exchange.symbols[coin_index])
+    last_price = tick.__getitem__("last")
+
+    return last_price
+
+
 async def main():
 
     exchange = await get_exchange()
 
+    coin_index = -1
+    coin = "BTC/USDT"
 
+    if coin in exchange.symbols:
+        coin_index = exchange.symbols.index(coin)
+    else:
+        print(coin + " not found in exchange!")
+
+    if coin_index > -1:
+
+        data_points = 10  # How many points to gather before training + prediction
+                          # Note that the rate limit of HitBTC is 1.5 seconds, so 10 will take 15 seconds!
+
+        prediction_amount = 10  # How many predictions to be made
+                                # This is temporary for testing, in the real product this would loop until interrupted
+                                # E.g., from the user telling the program to stop
+
+        for _ in range(prediction_amount):
+
+            price_list = []
+
+            for i in range(data_points):
+                price_list.append(await get_last_price(exchange, coin_index))
+                print("Gathered " + str(i + 1) + " / " + str(data_points) + " ...")
+
+            print(price_list)
 
     await exchange.close()
 
