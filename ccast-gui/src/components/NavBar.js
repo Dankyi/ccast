be569fd823f991ec4909dfdbcfa48f3a5bc54c6b
Adam Birch <32357719+U1761249@users.noreply.github.com>
2022-01-05 15:56:00 +0000

    Better NavBar and Organisation

--- ccast-gui/src/components/NavBar.js
+++ ccast-gui/src/components/NavBar.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import {
+    Nav,
+    NavLogo,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+} from "./NavbarElement";
 
-function NavBar() {
+const Navbar = () => {
     return (
-        <ul>
-            <li><Link to="/main">Main</Link></li>
-            <li><Link to="/login">Login</Link></li>
-        </ul>
-    )
-}
+        <>
+           <Nav className="navbar">
+            <NavLogo to="/">
+            <img src={"https://images.ctfassets.net/23aumh6u8s0i/3vCAkDy9os54mkVdD09lps/2b5386b4302f7ce38c4f6a69d6379dc0/reactjs"} width={85} height={85}/>
+            </NavLogo>
+            
+            <Bars />
 
-export default NavBar
+            <NavMenu>
+                <NavLink to="/" activeStyle>
+                    Home
+                </NavLink>
+                <NavLink to="/login" activeStyle>
+                    Login
+                </NavLink>
+                <NavLink to="/settings" activeStyle>
+                    Settings
+                </NavLink>
+            </NavMenu> 
+           </Nav> 
+        </>
+    );
+};
+export default Navbar;
