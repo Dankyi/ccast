233a30eeb8d40b21ae2d916c1019ddf22b68efbc
UggUggDev <10110halp@gmail.com>
2022-02-28 17:13:17 +0000

    Get current price of coin for grid

--- ccast-ai-testing/grid-bot-testing/autonomous_grid_bot.py
+++ ccast-ai-testing/grid-bot-testing/autonomous_grid_bot.py
@@ -14,9 +14,30 @@ async def get_exchange():
     return exchange
 
 
+async def get_current_price(exchange, coin_pair_id):
+
+    current_price = await exchange.fetch_ticker(exchange.symbols[coin_pair_id])
+    current_price = current_price.__getitem__("last")
+
+    return current_price
+
+
 async def main():
 
     exchange = await get_exchange()
+
+    coin_pair_id = -1
+    coin_pair = "ETH/BTC"
+
+    if coin_pair in exchange.symbols:
+        coin_pair_id = exchange.symbols.index(coin_pair)
+    else:
+        print(coin_pair + " not found in exchange!")
+
+    if coin_pair_id > -1:
+
+        current_price = await get_current_price(exchange, coin_pair_id)
+        print(current_price)
 
     await exchange.close()
 
