4a860ee6f6851e2e7864607e85da57450b918ba0
UggUggDev <10110halp@gmail.com>
2022-03-02 18:50:35 +0000

    Initialise template methods and whatnot

---
+++ ccast-ai-testing/grid-bot-testing/buy_sell_middleware.py
@@ -0,0 +1,21 @@
+import ccxt.async_support as ccxt
+import asyncio
+
+
+async def buy():
+    pass
+
+
+async def sell():
+    pass
+
+
+if __name__ == "__main__":
+
+    """
+    
+    This main method won't be here in the finished version of this code as its methods will be called from the AI
+    
+    """
+
+    pass
