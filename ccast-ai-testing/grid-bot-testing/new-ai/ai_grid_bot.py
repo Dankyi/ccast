73296f95d288c6fc105fa5b1e1ecbd6895795a1d
UggUggDev <10110halp@gmail.com>
2022-03-09 19:41:50 +0000

    Bot threading logic setup

--- ccast-ai-testing/grid-bot-testing/new-ai/ai_grid_bot.py
+++ ccast-ai-testing/grid-bot-testing/new-ai/ai_grid_bot.py
@@ -17,35 +17,34 @@ class AIGridBot(Thread):
         self.stop_signal = stop
         self.exchange = exchange
         self.coin_pair = coin_pair
-        self.market_loaded = False
 
     async def __start_ai(self):
 
-        if not self.market_loaded:
-            await self.exchange.load_markets(True)
-            self.market_loaded = True
-
+        await self.exchange.load_markets(True)
         await self.__run_ai()
 
     async def __run_ai(self):
-        pass
 
-    async def stop(self):
-        pass
+        while not self.stop_signal.is_set():
+            pass
+
+        await self.__stop_ai()
+
+    async def __stop_ai(self):
+
+        await self.exchange.close()
 
     def run(self):
 
-        while not self.stop_signal.is_set():
-            print("Hello")
-            sleep(1)
+        if operating_system().upper() == "WINDOWS":
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+        asyncio.run(self.__start_ai())
 
 
 if __name__ == "__main__":
 
     EXCHANGE = ccxt.binance({"verbose": False, "enableRateLimit": True})
-
-    if operating_system().upper() == "WINDOWS":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     stop_signal = Event()
 
@@ -54,7 +53,11 @@ if __name__ == "__main__":
 
     print("Press ENTER to STOP THE BOT!")
     print()
+
     ai_bot.start()
+
     _ = input()
+
     stop_signal.set()
+
     ai_bot.join()
