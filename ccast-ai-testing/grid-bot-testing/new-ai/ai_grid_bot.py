1f791d4595ba2cca03db3233141482a9f577aed9
UggUggDev <10110halp@gmail.com>
2022-03-09 19:30:49 +0000

    Initialise threaded bot (Flask will use this to start and stop the bot)

---
+++ ccast-ai-testing/grid-bot-testing/new-ai/ai_grid_bot.py
@@ -0,0 +1,58 @@
+import queue
+
+import ccxt.async_support as ccxt
+from platform import system as operating_system
+import asyncio
+from threading import Thread, Event
+from time import sleep
+
+import middleware_fake_money as middleware
+
+
+class AIGridBot(Thread):
+
+    def __init__(self, exchange, coin_pair, stop):
+
+        Thread.__init__(self)
+        self.stop_signal = stop
+        self.exchange = exchange
+        self.coin_pair = coin_pair
+        self.market_loaded = False
+
+    async def __start_ai(self):
+
+        if not self.market_loaded:
+            await self.exchange.load_markets(True)
+            self.market_loaded = True
+
+        await self.__run_ai()
+
+    async def __run_ai(self):
+        pass
+
+    async def stop(self):
+        pass
+
+    def run(self):
+
+        while not self.stop_signal.is_set():
+            print("Hello")
+            sleep(1)
+
+
+if __name__ == "__main__":
+
+    EXCHANGE = ccxt.binance({"verbose": False, "enableRateLimit": True})
+
+    if operating_system().upper() == "WINDOWS":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    stop_signal = Event()
+
+    ai_bot = AIGridBot(EXCHANGE, "ETH/BTC", stop_signal)
+    ai_bot.daemon = True
+
+    ai_bot.start()
+    sleep(5)
+    stop_signal.set()
+    ai_bot.join()
