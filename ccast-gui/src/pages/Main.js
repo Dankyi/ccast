82786f940cd4732beace3e807f2e5faf546da305
Adam Birch <32357719+U1761249@users.noreply.github.com>
2022-03-14 17:23:46 +0000

    Detect and display what state the AI should be in.

--- ccast-gui/src/pages/Main.js
+++ ccast-gui/src/pages/Main.js
@@ -5,13 +5,19 @@ function Main() {
     // Three states for trading: Idle, Dummy, and Live
     const [trading, setTrading] = useState('Idle')
 
+    const startReal = (e) => { setTrading('Live') }
+    const startFake = (e) => { setTrading('Dummy') }
+    const stopTrading = (e) => { setTrading('Idle') }
+
     return (
         <div className='Main'>
             <h1> Welcome to CCAST </h1>
+            <h3> AI trader is currently in {trading} mode. </h3>
+
             
-            <button > Start Trading!</button>
-            <button > Start Trading with fake money!</button>
-            <button > Stop Trading</button>
+            <button onClick={startReal}> Start Trading!</button>
+            <button onClick={startFake}> Start Trading with fake money!</button>
+            <button onClick={stopTrading}> Stop Trading</button>
             
         </div>
     )
