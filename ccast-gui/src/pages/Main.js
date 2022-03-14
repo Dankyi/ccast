b55ad049afe5aab0ee71d43d36e0837dea5ab411
Adam Birch <32357719+U1761249@users.noreply.github.com>
2022-03-14 17:02:04 +0000

    Added setup command. Get dependencies with yarn setup

--- ccast-gui/src/pages/Main.js
+++ ccast-gui/src/pages/Main.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Main() {
+
+    // Three states for trading: Idle, Dummy, and Live
+    const [trading, setTrading] = useState('Idle')
+
     return (
         <div className='Main'>
-            <h1> MAIN </h1>
+            <h1> Welcome to CCAST </h1>
+            
+            <button > Start Trading!</button>
+            <button > Start Trading with fake money!</button>
+            <button > Stop Trading</button>
+            
         </div>
     )
 }
